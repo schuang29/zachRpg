2aa7703c81c13cfb99dcca69b806fbd43f6a3bf1
ZacSC3 <zachary.chuang25@gmail.com>
2018-07-04 17:51:49 -0400

    added loop to prompt for commands

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,7 +1,28 @@
 import sys
 
-print ("Hello World!")
-x = input("Tell me your name: ")
-print ("Hello", x)
+commandList = ['? - Help', 'go - Move direction', 'exit - Exit game']
 
-print("Zac is a butt.")
+def listCommands(commandList):
+    print ('\tYou can do the folowing: ')
+    for x in commandList:
+        print ('\t', x)
+
+
+print ("Hello, welcome to our house!")
+
+
+name = input("What\'s you name? ")
+
+print("Hi", name)
+
+command = ''
+command = input("What would you like to do? ")
+
+while command != 'exit':
+    print ('\tYour command is ', command)
+
+    command = input('What\'s your next command?: ')
+
+    if command == '?': listCommands(commandList)
+
+print('See you later')
