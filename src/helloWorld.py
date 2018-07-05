044d41f0918fc0540205c044b0a0bbab93199249
ZacSC3 <zachary.chuang25@gmail.com>
2018-07-04 22:25:05 -0400

    Added inventory command and backpack array

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,6 +1,15 @@
 import sys
+import Weapons
 
-commandList = ['? - Help', 'go - Move direction', 'exit - Exit game']
+disk = Weapons.Weapon("Scratched Fortnite Disk", 2, "Blah")
+backpack = ['scratched fortnite disk']
+
+def showInv():
+    print ('You have ')
+    for stuff in backpack:
+        print(stuff)
+
+commandList = ['? - Help', 'go - Move direction', 'exit - Exit game', 'inventory - Open inventory']
 
 def listCommands(commandList):
     print ('\tYou can do the folowing: ')
@@ -19,10 +28,10 @@ command = ''
 command = input("What would you like to do? ")
 
 while command != 'exit':
-    print ('\tYour command is ', command)
+    if command == '?': listCommands(commandList)
+
+    if command == 'inventory': showInv()
 
     command = input('What\'s your next command?: ')
-
-    if command == '?': listCommands(commandList)
 
 print('See you later')
